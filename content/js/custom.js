932202fe9e8d8d4adb70b8d0a3f2732f77e064b1
Tsuiokuyo <Tsuiokuyo@gmail.com>
2024-08-15 11:09:55 +0800

    依時間更換背景

--- content/js/custom.js
+++ content/js/custom.js
@@ -1,14 +1,46 @@
-/* 輪播背景圖片 */
-var images = [
-		  "/background/background1.jpg",
-		  "/background/background2.jpg",
-		  "/background/background3.jpg",
-		  "/background/background4.jpg",
-		];
-var slideshow = images.sort(function() { return 0.5 - Math.random() });
+// /* 輪播背景圖片 */
+// var images = [
+// 		  "/background/background1.jpg",
+// 		  "/background/background2.jpg",
+// 		  "/background/background3.jpg",
+// 		  "/background/background4.jpg",
+// 		];
+// var slideshow = images.sort(function() { return 0.5 - Math.random() });
+// $(function () {
+// 	$.backstretch(slideshow,{ duration: 20000, fade: 1500 });
+// });
+
+var images = {
+  morning: "/background/background1.jpg",  // 早上 0600 ~ 1100
+  noon: "/background/background2.jpg",     // 中午 1100 ~ 1300
+  afternoon: "/background/background3.jpg", // 下午 1300 ~ 1700
+  evening: "/background/background4.jpg",  // 傍晚 1700 ~ 1900
+  night: "/background/background5.jpg",    // 晚上 1900 ~ 2400
+  lateNight: "/background/background6.jpg" // 入夜 2400 ~ 0600
+};
+
+function getBackgroundImage() {
+  var hour = new Date().getHours();
+  if (hour >= 6 && hour < 11) {
+      return images.morning;
+  } else if (hour >= 11 && hour < 13) {
+      return images.noon;
+  } else if (hour >= 13 && hour < 17) {
+      return images.afternoon;
+  } else if (hour >= 17 && hour < 19) {
+      return images.evening;
+  } else if (hour >= 19 && hour < 24) {
+      return images.night;
+  } else {
+      return images.lateNight;
+  }
+}
+
 $(function () {
-	$.backstretch(slideshow,{ duration: 20000, fade: 1500 });
+  var backgroundImage = getBackgroundImage();
+  $.backstretch(backgroundImage);
 });
+
 
 /* 拉姆蕾姆回到頂部或底部按鈕 */
 $(function() {
