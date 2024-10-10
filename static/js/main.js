929e995d0ad933717283a30701b30e107720483a
tsuiokuyo <tsuiokuyo@gmail.com>
2024-10-10 16:40:13 +0800

    fix bug

--- static/js/main.js
+++ static/js/main.js
@@ -972,7 +972,8 @@ let vue = new Vue({
             })
 */
 
-       /* this.$nextTick(function() {
+        this.$nextTick(function() {
+			/*
                 //用途不大的firebase字串加密QAQ
                 var _0x1d7d3f = _0x4320;
 
@@ -1102,7 +1103,7 @@ let vue = new Vue({
                         }
 
                     })
-
+				*/
 
                 this.getRandomArray();
                 let genres = []
@@ -1307,7 +1308,7 @@ let vue = new Vue({
                 }
             }
 
-        )*/
+        )
 
     },
     async mounted() {
